31b4442b4224a3c406255d329e215a040d2b0751
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-09-09 19:43:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+let themebtn = document.querySelector("#swap");
+let Tmode = "light" 
 
+themebtn.addEventListener("click", mode)
+function mode() {
+	if (Tmode==="light") {
+		Tmode = "dark"
+		 document.querySelector("body").style.backgroundColor = "black"
+	}
+	else{
+		Tmode="light"
+		 document.querySelector("body").style.backgroundColor = "white"
+	}
+}
